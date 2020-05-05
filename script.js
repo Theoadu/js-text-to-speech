a05479d5bd368f3f473d53615aa0fe48002ddb2a
Theophilus E.K Adukpo <theoadu@gmail.com>
2020-05-05 09:10:58 +0000

    Added js script file and selected all elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const playButton = document.getElementById("play-button");
+const pauseButton = document.getElementById("pause-button");
+const stopButton = document.getElementById("stop-button");
+const contentInput = document.getElementById("content");
+const speedInput = document.getElementById("speed");
+
+playButton.addEventListener("click", () => {});
